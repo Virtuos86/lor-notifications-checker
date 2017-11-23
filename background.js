b6c986ebd7b8a2bb3497cbfdf3de8764db1043d1
Virtuos86 <Virtuos86@yandex.ru>
2017-11-24 06:17:55 +0700

    Поправил кое-что. Но, в принципе, развивать дальше некуда. Кому нужно большего, смотрите LOR Button среди моих же репозиториев.

--- background.js
+++ background.js
@@ -1,30 +1,38 @@
+var ID = "LOR-NC";
 var target = 'https://www.linux.org.ru/notifications-count';
 var XHR = new XMLHttpRequest();
 
-function notificount( callback ) {
+function notificount(callback) {
     XHR.open("GET", target, true);
-    var timeout = setTimeout(() => { XHR.abort(); }, 10000);
-    XHR.onreadystatechange = () => {clearTimeout(timeout); callback(XHR);};
+    var timeout = setTimeout(function() { XHR.abort(); }, 10000);
+    XHR.onreadystatechange = function() { clearTimeout(timeout); callback(XHR); };
     XHR.send(null);
 };
 
 function note(title, message) {
-    browser.notifications.create({
-        "type": "basic",
-        //"iconUrl": browser.extension.getURL("icons/good-penguin.png"),
-        "title": title,
-        "message": message
-    });
+    chrome.notifications.create(
+        ID,
+        {
+            "type": "basic",
+            //"iconUrl": chrome.extension.getURL("icons/good-penguin.png"),
+            "title": title,
+            "message": message
+        }
+    );
+    setTimeout(function() { chrome.notifications.clear(ID); }, 3000);
 }
 
-function checkNotifications( xhr ) {
+function checkNotifications(xhr) {
     if( xhr.readyState == 4 ) {
         if(xhr.status == 200) {
             n = xhr.responseText;
-            if(parseInt(n) == 0)
+            if(parseInt(n) == 0) {
+                chrome.browserAction.setBadgeText({ text: "" });
                 note("Новых уведомлений нет", "");
-            else
+            } else {
+                chrome.browserAction.setBadgeText({ text: n });
                 note("Новые уведомления: " + n, "");
+            };
         };
     };
 };
@@ -35,12 +43,10 @@ function onError(error) {
   note("Ошибка!", "Не удалось открыть вкладку с уведомлениями");
 }
 
-browser.browserAction.onClicked.addListener(() => { notificount(checkNotifications); });
-browser.commands.onCommand.addListener((command) => {
-    if (command == "check-lor") {
-        notificount(checkNotifications);
-    } else if (command == "open-lor-notifications") {
-        var creating = browser.tabs.create({
+chrome.browserAction.onClicked.addListener(function() { notificount(checkNotifications); });
+chrome.commands.onCommand.addListener(function(command) {
+    if (command == "open-lor-notifications") {
+        var creating = chrome.tabs.create({
           url:"https://www.linux.org.ru/notifications"
         });
         creating.then(onCreated, onError);
